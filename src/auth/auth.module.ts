6fc450b6c32b9e34422ecc8f848f1254c253ebb2
Ronaldo <awademeronaldoo@gmail.com>
2025-10-27 19:13:16 +0100

    [UPDATE] Configuration du module JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,24 @@
+// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'VOTRE_SECRET_JWT',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
